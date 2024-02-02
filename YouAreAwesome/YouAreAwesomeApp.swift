204e298e9253a5145e31186b40f743b1c6243151
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-02-02 15:07:02 +0300

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Vefa Kosova on 2.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
