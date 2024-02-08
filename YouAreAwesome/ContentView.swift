d2ef8abce395f28a97af22cb149eb6c66192e71a
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-02-08 18:17:58 +0300

    .

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                    
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .italic()
+                .background(.yellow)
+                .padding()
+            
+            Button ("Press Me") {
+                message = "You Are Awesome!"
+            }
+            
         }
-        .padding()
     }
 }
 
