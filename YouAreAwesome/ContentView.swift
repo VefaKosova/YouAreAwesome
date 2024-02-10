f3f0bf6be92a3fe462f03cfa2c5736ba6782328e
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-02-10 21:49:15 +0300

    1.5

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,23 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = ""
+    @State private var message = "border frame infinty minimumscalefactor!"
     
     var body: some View {
         VStack {
-                    
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
                 .foregroundStyle(.red)
-                .italic()
-                .background(.yellow)
                 .padding()
+                .frame(width: 300, height: 150)
+                .border(.orange, width: 1)
             
-            Button ("Press Me") {
-                message = "You Are Awesome!"
+            HStack {
+                Button ("Awesome") {
+                    message = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button ("Great") {
+                    message = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
             }
-            
+            .border(.purple, width: 5)
         }
     }
 }
