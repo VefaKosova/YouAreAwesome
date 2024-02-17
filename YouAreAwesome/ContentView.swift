dfa295c74d32e2e94104b01540acca14d7e07bce
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-02-17 17:42:07 +0300

    1.8 scaletofit() scaletofill() clipped()

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,61 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "When the Genius Bar needs help, they call you!"
+    @State private var message = "Namaste"
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Text("You Have Skills!")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundStyle(Color("Gold-BC"))
-                    .padding()
-                    .background(Color("Maroon-BC"))
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-                
-                Spacer()
-                
-                Text(message)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                    .border(.orange, width: 1)
-                    .padding()
-                
-                Spacer()
-                
-                Divider()
-                    .background(.black)
-                    .padding()
-                    .frame(width: 150)
-                
-                Rectangle()
-                    .fill(.indigo)
-                    .frame(width: geometry.size.width * (2/3), height: 1)
-                
-                HStack {
-                    Button ("Awesome") {
-                        message = "You Are Awesome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button ("Great") {
-                        message = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
+        VStack {
+            
+            Spacer()
+            
+            Image(systemName: "figure.mind.and.body")
+                .resizable()
+                .scaledToFill()
+                .foregroundStyle(.pink)
+                .border(.pink)
+                .frame(width: 200, height: 300)
+                .clipped()
+                .border(.blue)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
                 .padding()
+                .border(.blue)
+            
+            Spacer()
+        
+            HStack {
+                Button ("Awesome") {
+                    message = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                Button ("Great") {
+                    message = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
             }
+            .padding()
         }
     }
 }
+
 
 #Preview {
     ContentView()
